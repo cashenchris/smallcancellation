9de5cd75cfa9c73a00b6456929f4898479286974
christopher_cashen <cashenchris@gmail.com>
2020-06-15 09:24:15 +0200

    comments, changed a variable name

--- smallcancellation.py
+++ smallcancellation.py
@@ -6,9 +6,11 @@ import itertools
 import networkx as nx
 import math
 
-def smallcancellation(relatorlist,theCp=None):
+def smallcancellation(relatorlist,theCprimebound=None):
     """
     Check if the relatorlist satisfy any of several small cancellation conditions that guarantee hyperbolicity.
+
+    If theCprimebound has already been computed for this relatorlist can input it to shortcircuit redundant computation.
 
     >>> smallcancellation(['abABcdCD']) # C'(1/6)
     True
@@ -24,12 +26,12 @@ def smallcancellation(relatorlist,theCp=None):
     F,rels=fg.parseinputwords(relatorlist)
     if not all(r==F.cyclic_reduce(r) for r in rels):
         raise ValueError("Relators are not cyclically reduced.")
-    if theCp is None:
-        theCp=Cprimebound(rels)
-    if theCp<Fraction(1,6):
+    if theCprimebound is None:
+        theCprimebound=Cprimebound(rels)
+    if theCprimebound<Fraction(1,6):
         return True
     theT=T(rels)
-    Cest=int(math.ceil(Fraction(theCp.denominator,theCp.numerator))) #C'(1/L) => C(L+1)
+    Cest=int(math.ceil(Fraction(theCprimebound.denominator,theCprimebound.numerator))) #C'(1/L) => C(L+1), quick check without computing C value
     if (Cest>=5 and theT>=4) or (Cest>=4 and theT>=5) or (Cest>=3 and theT>=7):
         return True
     theC=C(rels,7) # sometimes the C value is better than the estimate given by the C' value, compute it for real
@@ -52,18 +54,18 @@ def Cprimebound(relatorlist,Lambda=1):
         return 1
     rels.sort(key=len) # sort list of relators with shortest first
     irels=[rel for rel in itertools.chain.from_iterable(zip([w() for w in rels],[(w**(-1))() for w in rels]))] # arrange relators and inverses in a list of the form relator1, inverse of relator1, relator2, inverse of relator2,...
-    drels=[x+x for x in irels]
+    drels=[x+x for x in irels] # double the relators to look for pieces that would have wrapped
     for relatorindex in range(len(rels)):
         relator=irels[2*relatorindex]
         foundbiggest=False
         for L in range(len(relator),int(biggestratio*len(relator)),-1):# only check subwords of length L that would give biggest ratio if they were a piece
             for startingindex in range(len(relator)):
-                p=(relator+relator)[startingindex:startingindex+L] # the subword of length L starting at index i in reltaor as a cyclic word
+                p=(relator+relator)[startingindex:startingindex+L] # the subword of length L starting at index i in relator as a cyclic word
                 # now we need to check if p is a piece
                 # we do not need to check lower relatorindices, because we already scanned those relators for pieces
-                if any(p in x for x in [(relator+relator)[startingindex+1:len(relator)+startingindex+L-1]]+[drels[i] for i in range(2*relatorindex+1,len(drels))]):# found a matching subword, p is a piece
+                if any(p in x for x in [(relator+relator)[startingindex+1:len(relator)+startingindex+L-1]]+[drels[i] for i in range(2*relatorindex+1,len(drels))]):# look in this (doubled) relator at higher starting indices, and in all later relators, for other copies of p. If found a matching subword, p is a piece.
                     biggestratio=Fraction(len(p),len(relator))
-                    foundbiggest=True
+                    foundbiggest=True # we search pieces by decreasing length, so first one we find is longest
                     if biggestratio>=Fraction(1,Lambda):
                         return 1
                     break
@@ -79,13 +81,13 @@ def T(relatorlist):
     F,rels=fg.parseinputwords(relatorlist)
     if not all(r==F.cyclic_reduce(r) for r in rels):
         raise ValueError("Relators are not cyclically reduced.")
-    G=nx.Graph(wg.WGraph(rels))
+    G=nx.Graph(wg.WGraph(rels)) # compute the whitehead graph and then reduce it (to a simple Graph)
     theedges=[e for e in G.edges()]
     shortestcycle=float('inf')
     for e in theedges:
         G.remove_edge(*e)
         try:
-            shortestcycleusing_e=1+nx.shortest_path_length(G,*e)
+            shortestcycleusing_e=1+nx.shortest_path_length(G,*e) # compute distance between endpoints of e in G-e then add 1
         except nx.NetworkXNoPath:
             shortestcycleusing_e=float('inf')
         G.add_edge(*e)
@@ -106,7 +108,7 @@ def C(relatorlist,quit_at=float('inf')):
     thepieces=pieces(rels)
     minnumberpieces=quit_at
     def min_string_piece_expression(whatsleft,thepieces,quit_at):
-        # recursively determine the minimal expression of the string whatsleft as a concatenation of elements of thepieces, or stop once it is determined that any such expression requires at least quit_at pieces
+        # recursively determine the minimal expression of the string whatsleft as a concatenation of elements of thepieces, or stop once it is determined that any such expression requires at least quit_at many pieces
         # find a piece that agrees with a prefix of whatsleft and the recurse on the suffix
         if not whatsleft:
             return 0
@@ -118,7 +120,7 @@ def C(relatorlist,quit_at=float('inf')):
                 minexp=min(minexp,1+min_string_piece_expression(whatsleft[len(p):],thepieces,minexp-1))
         return minexp
     def min_relator_piece_expression(relator,thepieces,quit_at):
-        # This is first step in recursive search. Here we want to  find a piece p such that for relator r we can write p=xy and r=yzx, with y nontrivial.
+        # This is first step in recursive search. Here we want to  find a piece p such that for relator r we can write p=xy and r=yzx, with y nontrivial. That is, in this step only we think of r as cyclic word and allow first piece that wraps.
         r=relator()
         minexp=quit_at
         for p in thepieces:
